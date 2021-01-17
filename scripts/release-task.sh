4ac9ce574dfc2b6c6eb0c884e1a366b46e2cfcdf
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-17 13:44:11 +0000

    fix release task

--- scripts/release-task.sh
+++ scripts/release-task.sh
@@ -9,7 +9,10 @@ echo "2. Fetch repository ..."
 git fetch origin
 git reset --hard origin/master
 
-echo "3. Start docker instance"
+echo "3. Rebuild docker instance"
+sudo docker-compose build
+
+echo "4. Restart docker instance"
 sudo docker-compose up
 
 echo "# # # Finished release task # # #"
