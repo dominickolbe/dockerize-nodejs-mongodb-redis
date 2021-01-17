2d6be94a7b5c8f7ec65c163df4a12ef59f59c85d
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-17 13:30:04 +0000

    CS

--- scripts/release-task.sh
+++ scripts/release-task.sh
@@ -3,13 +3,13 @@
 echo "# # # Start release task # # #"
 
 echo "1. Stop docker instance"
-docker-compose down
+sudo docker-compose down
 
 echo "2. Fetch repository ..."
 git fetch origin
 git reset --hard origin/master
 
 echo "3. Start docker instance"
-docker-compose up
+sudo docker-compose up
 
 echo "# # # Finished release task # # #"
