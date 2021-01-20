66a4f9372dfa0df846f536ab68c8d8cad184dea4
Dominic Kolbe <kolbedominic@gmail.com>
2021-01-20 01:12:31 +0000

    CS

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_PORT = process.env.SERVER_PORT;
+export const SERVER_PORT = process.env.PORT;
 
 export const REDIS_HOST = `${process.env.REDIS_HOST}`;
 export const MONGO_HOST = `${process.env.MONGO_HOST}`;
